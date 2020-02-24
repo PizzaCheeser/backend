044ea3a714b2c05f7f1cc273e52f150ffadda21f
Prasnal <krasnal@lisp.pl>
2020-02-24 22:13:19 +0100

    feat: add slack connection

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import request, Response
+from flask import request, Response, jsonify
 
 from app.app import app
 from app.database.search import ES_search
@@ -30,4 +30,44 @@ def ingredients_choice():
 
         result = search.search_via_ingredients_postcode(must, must_not, code=post_code)
         return Response(headers={"Access-Control-Allow-Origin": '*'}, response=json.dumps(result), status=200)
+
+
+@app.route('/slack/get-pizzas', methods=['POST'])
+def slack_pizza():
+    es_settings = ES_config()
+    search = ES_search(es_settings)
+
+    if request.method == 'POST':
+        params = request.form['text']
+        args = params.split(';')
+        if len(args) == 3:
+            post_code = args[0]
+            wanted = [ingredient for ingredient in args[1].split(',') if ingredient]
+            not_wanted = [ingredient for ingredient in args[2].split(',') if ingredient]
+
+        else:
+            response = {
+                "response_type": "in_channel",
+                "text": "Please put parameters correctly: `/pizza <post-code>;<wanted>;<not-wanted>`"
+            }
+
+            return jsonify(response)
+
+        results = search.search_via_ingredients_postcode(wanted, not_wanted, code=post_code)
+        results_number = len(results)
+
+        result_string = f'I found {results_number} dreamed pizza! :) Bon appetit! \n \n'
+        for result in results:
+            restaurant_name = result['_id']
+            name=result['_source']['name']
+
+            ingredients = result['_source']['ingredients']
+            result_string += f'*Pizzeria name:* {restaurant_name} \n *Pizza name*: {name} \n *Ingredients*: {ingredients} \n \n'
+
+        response = {
+            "response_type": "in_channel",
+            "text": result_string
+        }
+
+        return jsonify(response)
 
