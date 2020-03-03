697ce16c6fb06e52c2521f4917b497c6b41f6b74
Prasnal <krasnal@lisp.pl>
2020-03-03 19:29:32 +0100

    remove using flask app in the init_database file

--- src/init_database.py
+++ src/init_database.py
@@ -1,5 +1,11 @@
 from app.database.base import ES_config
+import os
 
-database = ES_config()
+HOST = os.environ.get('HOST')
+PIZZERIA_ID = os.environ.get('ES_PIZZERIAS_ID')
+ES_PORT = os.environ.get('ES_PORT')
+ES_LOCATION_ID = os.environ.get('ES_LOCATION_ID')
+
+database = ES_config(HOST, PIZZERIA_ID, ES_PORT, ES_LOCATION_ID)
 database.delete_whole_database()
 database.create_structure()
