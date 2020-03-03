999518a372b635c12847c8838510dfac0df8bdc7
Prasnal <krasnal@lisp.pl>
2020-03-03 20:31:36 +0100

    change config to use envvariables

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
+import os
+
 DEBUG = True
-ES_PORT = 9200
-ES_PIZZERIAS_ID = "pizzerias"
-ES_LOCATION_ID = "locations"
-HOST = "elasticsearch"
+ES_PORT = os.environ.get("ES_PORT",9200)
+ES_PIZZERIAS_ID = os.environ.get("ES_PIZZERIAS_ID", "pizzerias")
+ES_LOCATION_ID = os.environ.get("ES_LOCATION_ID", "locations")
+HOST = os.environ.get("HOST", "elasticsearch")
