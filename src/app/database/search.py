b91004d6245032a6ca5030816a9c358a08687380
Mateusz Nowotyński <maxmati4@gmail.com>
2020-06-28 21:30:06 +0200

    Add prometheus metrics to debug search time issue

--- src/app/database/search.py
+++ src/app/database/search.py
@@ -1,4 +1,15 @@
 from app.exceptions.exceptions import SearchException, UnexpectedResult
+from prometheus_client import Summary
+
+CLEAN_RESULTS = Summary('pizza_clean_results_request_processing_seconds', 'Time spent cleaning pizza search results')
+SEARCH_INGREDIENTS = Summary('pizza_search_ingredients_request_processing_seconds',
+                             'Time spent processing search pizza request')
+SEARCH_INGREDIENTS_PREPARE_QUERY = Summary('pizza_search_ingredients_prepare_query_request_processing_seconds',
+                                           'Time spent preparing search pizza query')
+SEARCH_INGREDIENTS_EXECUTE_SEARCH = Summary('pizza_search_ingredients_execute_search_request_processing_seconds',
+                                            'Time spent executing search pizza query')
+SEARCH_INGREDIENTS_PARSE_RESULTS = Summary('pizza_search_ingredients_parse_results_request_processing_seconds',
+                                           'Time spent parsing search pizza results')
 
 
 class EsSearch:
@@ -166,46 +177,59 @@ class EsSearch:
             raise UnexpectedResult("More than one result")
         return results[0]["_source"]["timestamp"]
 
+    @SEARCH_INGREDIENTS.time()
     def search_via_ingredients_postcode(self, wanted, not_acceptable, code):
         '''
         Find all ingredients in the specific location
         '''
-
         if not wanted:
             wanted = list()
         if not not_acceptable:
             not_acceptable = list()
 
-        ingredients_query = self.__query_search_via_ingredients(wanted, not_acceptable)
-        postcode_query = self.__query_search_via_postcode(code)
-        bool_query = dict()
-        if ingredients_query:
-            bool_query.update({'must': ingredients_query})
-        if postcode_query:
-            bool_query.update({"filter": postcode_query})
+        @SEARCH_INGREDIENTS_PREPARE_QUERY.time()
+        def prepare_query():
+            ingredients_query = self.__query_search_via_ingredients(wanted, not_acceptable)
+            postcode_query = self.__query_search_via_postcode(code)
+            bool_query = dict()
+            if ingredients_query:
+                bool_query.update({'must': ingredients_query})
+            if postcode_query:
+                bool_query.update({"filter": postcode_query})
 
-        query = {
-            "size": 1010,
-            "_source": "false",
-            "query": {
-                "bool": bool_query
+            return {
+                "size": 1010,
+                "_source": "false",
+                "query": {
+                    "bool": bool_query
+                }
             }
-        }
 
-        try:
-            result = self.es.search(index=self.pizzerias_id, body=query)['hits']['hits']
-        except Exception as e:
-            raise SearchException("Searching via ingredients and postcode failed") from e
+        query = prepare_query()
 
-        pizzas_list = list()
-        if len(result) > 0:
-            for i in result:
-                pizzas_list.extend(i['inner_hits']['pizza']['hits']['hits'])
-        else:
-            # no matching pizzas
+        @SEARCH_INGREDIENTS_EXECUTE_SEARCH.time()
+        def execute_search():
+            try:
+                return self.es.search(index=self.pizzerias_id, body=query)['hits']['hits']
+            except Exception as e:
+                raise SearchException("Searching via ingredients and postcode failed") from e
+
+        result = execute_search()
+
+        @SEARCH_INGREDIENTS_PARSE_RESULTS.time()
+        def parse_results():
             pizzas_list = list()
-        return pizzas_list
+            if len(result) > 0:
+                for i in result:
+                    pizzas_list.extend(i['inner_hits']['pizza']['hits']['hits'])
+            else:
+                # no matching pizzas
+                pizzas_list = list()
+            return pizzas_list
 
+        return parse_results()
+
+    @CLEAN_RESULTS.time()
     def __clean_matched_pizzas(self, results):
         new_results = [
             {
