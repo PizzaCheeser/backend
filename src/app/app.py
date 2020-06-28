a647b0a366bf071200e6a2e9428d0c12d4d4b26d
Mateusz Nowotyński <maxmati4@gmail.com>
2020-06-28 21:30:06 +0200

    Add prometheus metrics to debug search time issue
    Signed-off-by: Mateusz Nowotyński <maxmati4@gmail.com>

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, defaults_prefix="pizza")
 CORS(app)
