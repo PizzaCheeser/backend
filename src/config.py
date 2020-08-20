d901002d3306ad2c7cb7334bbd708639e3012904
Prasnal <krasnal@lisp.pl>
2020-08-20 22:21:20 +0200

    added price per cm²

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ import os
 SENTRY_DSN = os.environ.get("SENTRY_DSN", "")
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 ES_PORT = os.environ.get("ES_PORT", 9200)
-ES_PIZZERIAS_ID = os.environ.get("ES_PIZZERIAS_ID", "pizzerias2")
-ES_LOCATION_ID = os.environ.get("ES_LOCATION_ID", "locations2")
+ES_PIZZERIAS_ID = os.environ.get("ES_PIZZERIAS_ID", "pizzerias")
+ES_LOCATION_ID = os.environ.get("ES_LOCATION_ID", "locations")
 HOST = os.environ.get("HOST", "elasticsearch")
 
 MIN_DELAY_BETWEEN_SCRAPING = os.environ.get("MIN_DELAY_BETWEEN_SCRAPING", 86400)
