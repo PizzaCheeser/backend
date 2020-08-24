236f7e40dfe9ce383dacff9c991df43955ea21b8
Mateusz Nowotyński <maxmati4@gmail.com>
2020-08-25 01:05:24 +0200

    Handle pizzas scraper errors
    Signed-off-by: Mateusz Nowotyński <maxmati4@gmail.com>

--- src/ScraperDatabaseConnector.py
+++ src/ScraperDatabaseConnector.py
@@ -26,6 +26,7 @@ class ScraperDatabaseConnector:
         self.pizzerias = EsPizzerias(es_settings)
         self.search = EsSearch(es_settings)
         self.validator = Validator()
+        self.MIN_DELAY = int(app.config['MIN_DELAY_BETWEEN_SCRAPING'])
 
     def scrape_locations(self, url=None):
         '''
@@ -54,7 +55,7 @@ class ScraperDatabaseConnector:
                 except Exception as err:
                     app.logger.error(f"Failed to scrape location url=\"{link}\". Err=\"{err}\"")
                     with sentry_sdk.push_scope() as scope:
-                        scope.set_extra(url, link)
+                        scope.set_extra("url", link)
                         sentry_sdk.capture_exception(err)
 
     def main(self, city=None):
@@ -63,11 +64,22 @@ class ScraperDatabaseConnector:
         If the pizzeria is already in the database and was scraped recently, it only updates delivery postcode filed
         otherwise it scrapes pizzeria website and inserts pizzeria wit all validated pizzas to the database
         '''
-        min_delay = int(app.config['MIN_DELAY_BETWEEN_SCRAPING'])
         locations_list = self.location.get_location(city=city, empty=False)
         for location in locations_list:
-            all_pizzerias = self.restaurantScraper.get_all_pizzerias(location['link'])
-            for pizzeria in all_pizzerias:
+            with sentry_sdk.push_scope() as scope:
+                scope.set_extra("location", location)
+                try:
+                    all_pizzerias = self.restaurantScraper.get_all_pizzerias(location['link'])
+                    for pizzeria in all_pizzerias:
+                        self.scrape_pizzeria(location, pizzeria)
+                except Exception as err:
+                    app.logger.error(f"Failed fetch pizzerias from location=\"{location}\". Err=\"{err}\"")
+                    sentry_sdk.capture_exception(err)
+
+    def scrape_pizzeria(self, location, pizzeria):
+        with sentry_sdk.push_scope() as scope:
+            scope.set_extra("pizzeria", pizzeria)
+            try:
                 pizzeria_id = pizzeria['endpoint'].split('/')[-1]
                 if self.pizzerias.check_if_exists(pizzeria_id):
                     self.pizzerias.update_postcode(pizzeria_id, location['postcode'])
@@ -77,8 +89,8 @@ class ScraperDatabaseConnector:
                     self.pizzerias.update_field(pizzeria_id, "timestamp", timestamp_now)
 
                     delay = timestamp_now - timestamp
-                    if delay < min_delay:
-                        continue
+                    if delay < self.MIN_DELAY:
+                        return
                 url = self.restaurantScraper.url + pizzeria['endpoint']
                 data = self.restaurantScraper.get_pizzeria_data(
                     pizzeria_id=pizzeria_id,
@@ -89,9 +101,22 @@ class ScraperDatabaseConnector:
 
                 self.pizzerias.insert_pizzeria(data)
                 for pizza in self.restaurantScraper.get_pizza(url):
-                    validated_ingredients = self.validator.pizza_validator(pizza)
-                    pizza.update({"validated_ingredients": validated_ingredients})
-                    self.pizzerias.insert_pizza(pizzeria_id, pizza)
+                    self.scrape_pizza(location, pizza, pizzeria, pizzeria_id)
+            except Exception as err:
+                app.logger.error(f"Failed fetch pizzeria=\"{pizzeria}\" in location=\"{location}\". Err=\"{err}\"")
+                sentry_sdk.capture_exception(err)
+
+    def scrape_pizza(self, location, pizza, pizzeria, pizzeria_id):
+        with sentry_sdk.push_scope() as scope:
+            scope.set_extra("pizza", pizza)
+            try:
+                validated_ingredients = self.validator.pizza_validator(pizza)
+                pizza.update({"validated_ingredients": validated_ingredients})
+                self.pizzerias.insert_pizza(pizzeria_id, pizza)
+            except Exception as err:
+                app.logger.error(
+                    f"Failed fetch pizza=\"{pizza}\" pizzeria=\"{pizzeria}\" in location=\"{location}\". Err=\"{err}\"")
+                sentry_sdk.capture_exception(err)
 
 
 if __name__ == '__main__':
